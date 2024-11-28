e823f8f273e052971fd0904f05734b3fca061810
Mehdib1-oss <mehdi.seghier@laplateforme.io>
2024-11-28 10:17:57 +0100

    alphabet python

---
+++ alphabet/gg.py
@@ -0,0 +1,19 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+print(alphabet)
+
+alphabet2 ="zyxwvutsrqponmlkihgfedcba"
+print(alphabet2)
+
+ma_string="je suis une string"
+print(ma_string)
+
+num1=40
+num2=2
+s=num1+num2
+print(s)
+
+num3=3
+num4=14
+s=3*14
+print(s)
+
